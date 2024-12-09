8dc419f6752e05552d8aca7ef5127547a30d42fb
Shahezad khan <khanshahezad2002@gmail.com>
2024-12-09 18:40:24 +0530

    Update Program.cs

--- FastFood/FastFood/Program.cs
+++ FastFood/FastFood/Program.cs
@@ -6,7 +6,7 @@ namespace FastFood
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            
+            // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
